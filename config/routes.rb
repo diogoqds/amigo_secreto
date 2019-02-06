1fa2abc6d0bd544a460b8f68e58ffe54b64ee71d
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-02-06 19:56:16 -0200

    generate controllers and raffle mailer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  get 'members/create'
+
+  get 'members/destroy'
+
+  get 'members/update'
+
+  get 'campaigns/index'
+
+  get 'campaigns/create'
+
+  get 'campaigns/update'
+
+  get 'campaigns/destroy'
+
+  get 'pages/home'
+
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount Sidekiq::Web => '/sidekiq'
 end
